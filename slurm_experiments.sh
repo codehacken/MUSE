a045ff8d1c51f3a04403b70403aeafd9287dd6b0
gashwin1 <gashwin1@taki-usr1.cm.cluster>
2020-07-14 13:53:00 -0400

    Low resource settings jobs.

--- slurm_experiments.sh
+++ slurm_experiments.sh
@@ -4,6 +4,7 @@
 # Run all experiments related to BiFNN for word alignment.
 SEED=0
 input=$1
+ITERATE=${2:-"False"}
 MEM=40000
 
 # Start processing.
@@ -15,16 +16,17 @@ do
     SRC_EMB=`echo $LINE | awk -F " " '{print $3}'`
     TGT_EMB=`echo $LINE | awk -F " " '{print $4}'`
     NUM_LAYERS=`echo $LINE | awk -F " " '{print $5}'`
-    echo "JOB: $SRC_LANG, $TGT_LANG, $SRC_EMB, $TGT_EMB"
+    VOCAB_SIZE=`echo $LINE | awk -F " " '{print $6}'`
+    echo "JOB: $SRC_LANG, $TGT_LANG, $SRC_EMB, $TGT_EMB, $VOCAB_SIZE"
 
     # Baseline alignment.
     sbatch --account=pi_ferraro --job-name=$SRC_LANG-$TGT_LANG-$NUM_LAYERS-b --mem=$MEM --qos=medium+ \
-           bidnn.slurm $SRC_LANG $TGT_LANG $SRC_EMB $TGT_EMB $NUM_LAYERS "baseline" $SEED
+           bidnn.slurm $SRC_LANG $TGT_LANG $SRC_EMB $TGT_EMB $NUM_LAYERS "baseline" $SEED $ITERATE $VOCAB_SIZE
 
     sbatch --account=pi_ferraro --job-name=$SRC_LANG-$TGT_LANG-$NUM_LAYERS-bdma --mem=$MEM --qos=medium+ \
-           bidnn.slurm $SRC_LANG $TGT_LANG $SRC_EMB $TGT_EMB $NUM_LAYERS "bdma" $SEED
+           bidnn.slurm $SRC_LANG $TGT_LANG $SRC_EMB $TGT_EMB $NUM_LAYERS "bdma" $SEED $ITERATE $VOCAB_SIZE
 
     sbatch --account=pi_ferraro --job-name=$SRC_LANG-$TGT_LANG-$NUM_LAYERS-bdsh --mem=$MEM --qos=medium+ \
-           bidnn.slurm $SRC_LANG $TGT_LANG $SRC_EMB $TGT_EMB $NUM_LAYERS "bdma-shared" $SEED
+           bidnn.slurm $SRC_LANG $TGT_LANG $SRC_EMB $TGT_EMB $NUM_LAYERS "bdma-shared" $SEED $ITERATE $VOCAB_SIZE
 
 done < "$input"
